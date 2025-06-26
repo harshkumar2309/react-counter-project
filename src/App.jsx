07c33ac3fd86db46e2367afe45ff751c6d4bdb13
Harsh Kumar <yadavharsh1201@gmail.com>
2025-06-26 13:03:50 +0530

    add reset counter button

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,37 @@ import './App.css'
 
 function App() {
 
-  let [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(0)
 
   const addValue = () => {
     setCounter(counter + 1)
-    console.log("clicked", counter);
+    console.log("clicked", counter + 1);
   }
 
   const removeValue = () => {
     setCounter(counter - 1);
-    console.log("clicked", counter);
+    console.log("clicked", counter - 1);
   };
+
+  const resetValue = () => {
+    setCounter(0); 
+    console.log("clicked",0);  
+  }
 
   return (
     <>
       <h1>Update Counter</h1>
       <h2>Counter value: {counter}</h2>
 
-      <button onClick={addValue}>Add value: {counter}</button>
+      <button onClick={addValue}>Add +1 </button>
 
       <br />
 
-      <button onClick={removeValue}>Remove value: {counter}</button>
+      <button onClick={removeValue}>Remove -1 </button>
+
+      <br />
+
+      <button onClick={resetValue}>Reset to 0 </button>
     </>
   );
 }
